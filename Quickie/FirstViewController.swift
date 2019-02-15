c2f2770494c2dabb2321b9866338ab352b920e99
Paweł Ambrożej <Amper@pawel-macbook-air.local>
2019-02-15 17:14:36 +0000

    Initial Commit

---
+++ Quickie/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Quickie
+//
+//  Created by Paweł Ambrożej on 15/02/2019.
+//  Copyright © 2019 Pawel Ambrozej. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Quickie/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Quickie
+//
+//  Created by Paweł Ambrożej on 15/02/2019.
+//  Copyright © 2019 Pawel Ambrozej. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
